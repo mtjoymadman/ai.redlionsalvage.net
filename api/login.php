a11906fcb8f8b007b2e4c914e30ae00e7815fa27
mtjoymadman <52000816+mtjoymadman@users.noreply.github.com>
2025-03-02 20:30:03 -0500

    Update login.php

--- api/login.php
+++ api/login.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-// require_once 'config.php';  // Relative path within api/
+// require_once 'config.php';
 
 $response = ['success' => false, 'message' => ''];
 
